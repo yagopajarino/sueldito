8cb8bfeeb08767a033362479c1cda939efae49db
Yago Pajariño <yagopajarino@outlook.com>
2022-12-22 14:51:01 -0300

    Update App.js and add styling properties

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Form from "./components/Form.js";
+import Results from "./components/Results.js";
+import Donaciones from "./components/Donaciones.js";
+import Footer from "./components/Footer.js";
+
 function App() {
+  const [sueldo, setSueldo] = useState(0);
+  const [fecha, setFecha] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+
+  useEffect(() => {
+    console.log(sueldo);
+  }, [sueldo]);
+
+  useEffect(() => {
+    setShowResults(sueldo != 0);
+  }, [sueldo]);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div className="w-full h-screen justify-between flex items-center flex-col">
+      <div className="w-full flex items-center flex-col">
+        <h1 className="text-5xl py-8 text-center">Sueldito</h1>
+        <p className="text-center">
+          Calculá cuanto deberías estar cobrando hoy 🥲
+        </p>
+
+        {showResults ? (
+          <Results />
+        ) : (
+          <Form setSueldo={setSueldo} setFecha={setFecha} />
+        )}
+      </div>
+      <div className="w-full">
+        <Donaciones />
+        <Footer />
+      </div>
     </div>
   );
 }
