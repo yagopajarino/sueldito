3656259036fbeec29dd78ae11aa9814c0672ece8
Yago Pajariño <yagopajarino@outlook.com>
2022-12-23 16:48:37 -0300

    Add api call in App.js and change style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Form from "./components/Form.js";
 import Results from "./components/Results.js";
 import Donaciones from "./components/Donaciones.js";
 import Footer from "./components/Footer.js";
+import api from "./api/api.js";
 
 function App() {
   const [sueldo, setSueldo] = useState(0);
@@ -10,8 +11,8 @@ function App() {
   const [showResults, setShowResults] = useState(false);
 
   useEffect(() => {
-    console.log(sueldo);
-  }, [sueldo]);
+    api.ping();
+  });
 
   useEffect(() => {
     setShowResults(sueldo != 0);
@@ -21,17 +22,29 @@ function App() {
     <div className="w-full h-screen justify-between flex items-center flex-col">
       <div className="w-full flex items-center flex-col">
         <h1 className="text-5xl py-8 text-center">Sueldito</h1>
-        <p className="text-center">
-          Calculá cuanto deberías estar cobrando hoy 🥲
+        <p className="text-center text-xl font-light">
+          Conocé cuanto deberías cobrar ajustando tu sueldo pasado por
+          inflación, ripte, dolar oficial y paralelo.
         </p>
 
         {showResults ? (
-          <Results />
+          <>
+            <Results fecha={fecha} sueldo={sueldo} />
+            <button
+              onClick={() => {
+                setShowResults(false);
+                setSueldo(0);
+              }}
+              className="w-1/2"
+            >
+              Volver
+            </button>
+          </>
         ) : (
           <Form setSueldo={setSueldo} setFecha={setFecha} />
         )}
       </div>
-      <div className="w-full">
+      <div className="w-full mt-5">
         <Donaciones />
         <Footer />
       </div>
