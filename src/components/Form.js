cbef242276f790748f8ed049866cc4536facdda0
Yago Pajariño <yagopajarino@outlook.com>
2023-01-06 12:05:18 -0300

    Add date validation to Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,10 +5,27 @@ const Form = ({ setSueldo, setFecha }) => {
     const s = document.querySelector("#sueldo");
     const f = document.querySelector("#fecha");
     s.value == "" ? s.classList.add("error") : s.classList.remove("error");
-    f.value == "" ? f.classList.add("error") : f.classList.remove("error");
-    if (f.value != "" && s.value != "") {
+    !validDate(f.value)
+      ? f.classList.add("error")
+      : f.classList.remove("error");
+    if (s.value != "" && validDate(f.value)) {
       setSueldo(s.value);
       setFecha(f.value);
+    }
+  };
+
+  const validDate = (date_string) => {
+    const dateRegex =
+      /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+    if (date_string.match(dateRegex)) {
+      const today = new Date();
+      const date = Date.parse(date_string);
+      const desde = new Date(2017, 1, 1);
+      const hasta = new Date();
+      hasta.setMonth(today.getMonth() - 2);
+      return desde < date && date < hasta;
+    } else {
+      return false;
     }
   };
 
